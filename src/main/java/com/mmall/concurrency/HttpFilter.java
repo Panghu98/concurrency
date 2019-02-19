a343bbd3ec2c6f8e5492ea37315fe787055f7053
panghu <123@panghu.com>
2019-02-19 22:27:39 +0800

    ThreadLocal

---
+++ src/main/java/com/mmall/concurrency/HttpFilter.java
@@ -0,0 +1,39 @@
+package com.mmall.concurrency;
+
+
+import com.mmall.concurrency.example.threadLocal.RequestHolder;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+/**
+ * @author panghu
+ * @Title: HttpFilter
+ * @ProjectName cuncurrency_demo
+ * @Description: TODO
+ * @date 19-2-19 下午9:02
+ */
+@Slf4j
+public class HttpFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        /*       request.getSession().getAttribute("attribute"); */
+        log.info("do filter,{},{}",Thread.currentThread().getId(),request.getServletPath());
+        RequestHolder.add(Thread.currentThread().getId());
+        filterChain.doFilter(servletRequest,servletResponse);
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
---
+++ src/main/java/com/mmall/concurrency/example/threadLocal/ThreadLocalController.java
@@ -0,0 +1,24 @@
+package com.mmall.concurrency.example.threadLocal;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @author panghu
+ * @Title: ThreadLocalController
+ * @ProjectName cuncurrency_demo
+ * @Description: TODO
+ * @date 19-2-19 下午10:02
+ */
+@Controller
+@RequestMapping("/threadLocal")
+public class ThreadLocalController {
+
+    @RequestMapping("/test")
+    @ResponseBody
+    public Long test(){
+        return RequestHolder.getId();
+    }
+
+}
