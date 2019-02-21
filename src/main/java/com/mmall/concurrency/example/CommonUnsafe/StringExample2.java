381229416f521a56ae3ac33b3ad215635e74a717
panghu <123@panghu.com>
2019-02-21 16:42:45 +0800

    Set集合

---
+++ src/main/java/com/mmall/concurrency/example/CommonUnsafe/StringExample2.java
@@ -0,0 +1,52 @@
+package com.mmall.concurrency.example.CommonUnsafe;
+
+import com.mmall.concurrency.annoations.ThreadSafe;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+
+/**
+ * StringBuilder
+ * StringBuffer基本上所有的方法都是添加了synchronized关键字的(除了构造方法)
+ * @author panghu
+ */
+@Slf4j
+@ThreadSafe
+public class StringExample2 {
+
+    // 请求总数
+    public static int clientTotal = 5000;
+
+    // 同时并发执行的线程数
+    public static int threadTotal = 200;
+
+    public static StringBuffer stringBuffer = new StringBuffer();
+
+    public static void main(String[] args) throws Exception {
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        final Semaphore semaphore = new Semaphore(threadTotal);
+        final CountDownLatch countDownLatch = new CountDownLatch(clientTotal);
+        for (int i = 0; i < clientTotal ; i++) {
+            executorService.execute(() -> {
+                try {
+                    semaphore.acquire();
+                    update2();
+                    semaphore.release();
+                } catch (Exception e) {
+                    log.error("exception", e);
+                }
+                countDownLatch.countDown();
+            });
+        }
+        countDownLatch.await();
+        executorService.shutdown();
+        log.info("count:{}", stringBuffer.length());
+    }
+
+    private static void update2() {
+        stringBuffer.append("1");
+    }
+}
