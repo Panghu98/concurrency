d9cce3c5f31506eada6a3fb0d5458d8751640a2b
panghu <792559738@qq.com>
2019-08-25 08:57:21 +0800

    增加volatile关键字

---
+++ src/main/java/com/mmall/concurrency/thread/VolatileFeaturesExample.java
@@ -0,0 +1,34 @@
+package com.mmall.concurrency.thread;
+
+/**
+ * @author panghu
+ */
+public class VolatileFeaturesExample {
+
+     long v1 =0L;
+
+    /**
+     * 对单个的普通变量的写用同一个锁同步
+     * @param l
+     */
+    public synchronized void set(long l){
+        v1 = 1;
+    }
+
+    public void getAndIncrement(){
+        long temp = get();
+        temp += 1L;
+        set(temp);
+    }
+
+    /**
+     * 对单个的普通变量的读用同一个锁同步
+     * @return
+     */
+    public synchronized long get(){
+        return v1;
+    }
+
+
+
+}
