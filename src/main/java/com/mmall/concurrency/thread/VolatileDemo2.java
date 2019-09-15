c483bafa27a8198860c2b5c420e052c4843213e2
panghu <792559738@qq.com>
2019-09-15 21:13:37 +0800

    增加对volatile的理解

--- src/main/java/com/mmall/concurrency/thread/VolatileDemo2.java
+++ src/main/java/com/mmall/concurrency/thread/VolatileDemo2.java
@@ -1,19 +1,16 @@
 package com.mmall.concurrency.thread;
 
-import java.util.concurrent.atomic.AtomicInteger;
-
 public class VolatileDemo2 implements Runnable{
 
     private static int a = 0;
 
     private static synchronized void plus(){
             a++;
-            System.out.println("线程"+Thread.currentThread().getName()+"执行,当前a的值为"+a);
     }
 
     @Override
     public void run() {
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < 500; i++) {
             plus();
         }
     }
@@ -23,9 +20,17 @@ public class VolatileDemo2 implements Runnable{
         Thread thread1 = new Thread(volatileDemo);
         Thread thread2 = new Thread(volatileDemo);
         Thread thread3 = new Thread(volatileDemo);
+        Thread thread4 = new Thread(volatileDemo);
+        Thread thread5 = new Thread(volatileDemo);
         thread1.start();
         thread2.start();
         thread3.start();
+        thread4.start();
+        thread5.start();
+        System.out.println("线程"+Thread.currentThread().getName()+"执行,当前a的值为"+a);
+        System.err.println(a);
+        System.out.println("线程"+Thread.currentThread().getName()+"执行,当前a的值为"+a);
+
     }
 
 }
